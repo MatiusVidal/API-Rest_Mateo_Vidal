f1857f41b184326b5921271933b2f6bb11ac488d
MatiusVidal <89400953+MatiusVidal@users.noreply.github.com>
2022-08-01 20:18:44 -0500

    Primer commit

---
+++ src/main/java/com/example/APIREST_mateoVidal/ApirestMateoVidalApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIREST_mateoVidal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestMateoVidalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestMateoVidalApplication.class, args);
+	}
+
+}
